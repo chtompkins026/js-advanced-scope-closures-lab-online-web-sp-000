8db5570b2f7fbdd8ceff991ee2d07cf65cb2998d
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-29 17:01:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-produceDrivingRange(d1, d2){ 
-   return function(c) {
+function produceDrivingRange(c){ 
+   return function(d1,d2) {
     let myRe = /[0-9]/g;
     let a1 = d1.match(myRe).join('');
     let a2 = d2.match(myRe).join('');
     let b = Math.abs(a1-a2); 
     if (b > c) { 
-      return `within range by ${b-c}`; 
+      return `${b-c} blocks out of range`; 
     } else {
-      return `${c-b} out of range`;
+      return `within range by ${c-b}`;
     }
   };
 }
 
-it('takes an argument of blockRange which is then used to calculate if something is within range', function(){
-      let eightBlockRange = produceDrivingRange(8)
-      expect(eightBlockRange('10th', '20th')).to.equal('2 blocks out of range')
-      expect(eightBlockRange('10th', '14th')).to.equal('within range by 4')
-    })
