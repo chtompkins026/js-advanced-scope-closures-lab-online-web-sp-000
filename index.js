427e576ca040abbabee4e141b72e4c3298ed5e01
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-29 16:54:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-produceDrivingRange(){ 
-   return function(marketMultiplier) {
-    return marketMultiplier * manufacturePrice;
+produceDrivingRange(d1, d2){ 
+   return function(checker) {
+    
   };
 }
+
+it('takes an argument of blockRange which is then used to calculate if something is within range', function(){
+      let eightBlockRange = produceDrivingRange(8)
+      expect(eightBlockRange('10th', '20th')).to.equal('2 blocks out of range')
+      expect(eightBlockRange('10th', '14th')).to.equal('within range by 4')
+    })
